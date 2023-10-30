773d14b129e6140a4d28306cede9bba1c8545291
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2023-10-30 10:55:06 +0900

    [Gold V] Title: 평범한 배낭, Time: 436 ms, Memory: 112276 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -3,13 +3,10 @@ input = sys.stdin.readline
 N,K = map(int,input().split())
 I = [[*map(int, input().split())] for _ in range(N)] #W, V
 I = [[0,0]]+I
-memo = [[-1]*(K+1) for _ in range(2)]
+memo = [[0]*(K+1) for _ in range(2)]
 
 for itemIdx in range(len(I)):
     for capacity in range(K+1):  
-        if capacity==0 or itemIdx==0:
-            memo[itemIdx%2][capacity]=0
-            continue
         ip=itemIdx%2
         r = memo[ip-1][capacity] #아이템 안 썼을 떄
         if capacity - I[itemIdx][0] >=0 :
