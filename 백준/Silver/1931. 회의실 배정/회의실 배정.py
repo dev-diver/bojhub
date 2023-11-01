1130bb1861b05b2f7929dbb795d1cf3b89d9bbcd
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2023-11-01 23:58:19 +0900

    [Silver I] Title: 회의실 배정, Time: 284 ms, Memory: 67756 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,13 +1,20 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(int(1e9))
 
 N = int(input())
 T = [[*map(int,input().split())] for _ in range(N)]
 T.sort(key=lambda x:(x[1],x[0]))
-end = 0
-r=0
-for Ts,Te in T:
-    if Ts >= end:
-        r+=1
-        end=Te
-print(r)
+
+def maxC(m):
+    end = T[m][1]
+    k=m
+    for i in range(m+1,N):
+        if(T[i][0]>=end):
+            k=i
+            break
+    if k!=m:
+        return 1+maxC(k)
+    return 0
+
+print(maxC(0)+1)
