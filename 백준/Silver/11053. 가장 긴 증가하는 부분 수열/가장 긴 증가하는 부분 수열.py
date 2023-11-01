900d6948e76a2bb9118c9cff3725b5f71eeab6bb
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2023-11-01 20:48:42 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 124 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,20 +1,16 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(int(1e9))
 
 N = int(input())
 *S, = map(int,input().split())
 S = [0]+S
 M = [0]*(N+1)
 
-def lis(i):
-    if M[i]!=0: return M[i]
-    r=0
+for i in range(N,-1,-1):
+    r = 0
     for k in range(i+1,N+1):
         if(S[k]>S[i]):
-            r = max(r,lis(k))
-    r+=1
-    M[i]=r
-    return r
+            r = max(r, M[k])
+    M[i]=r+1
 
-print(lis(0)-1)
+print(M[0]-1)
