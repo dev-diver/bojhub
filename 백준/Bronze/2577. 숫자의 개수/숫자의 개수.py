1d4adfb2acdc8c619cfa4dbc21cb1fe77146ebd0
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2023-11-17 10:32:52 +0900

    [Bronze II] Title: 숫자의 개수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,8 +1,5 @@
-a= int(input())
-b= int(input())
-c= int(input())
-res = list(str(a*b*c))
-ans = [0 for x in range(10)]
-for x in res:
-  ans[int(x)]+=1
-print('\n'.join(map(str,ans)))
+S=[*str(int(input())*int(input())*int(input()))]
+cnt=[0]*10
+for s in S:
+    cnt[int(s)]+=1
+print(*cnt, sep="\n")
