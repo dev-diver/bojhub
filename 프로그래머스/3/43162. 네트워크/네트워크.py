5b564549f367ee8c5a6bba6c06e2eb360e065fef
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-03-28 18:09:52 +0900

    [level 3] Title: 네트워크, Time: 0.27 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,19 +1,17 @@
 def solution(n, computers):
     
-    stk = [(i,True) for i in range(n)]
     vis = [False for _ in range(n)]
+    def dfs(c):
+        if(vis[c]):
+            return 0
+        vis[c] = True
+        for next_c, conn in enumerate(computers[c]):
+            if(conn):
+                dfs(next_c)
+        return 1
     
     cnt = 0
-    while stk:
-        c, isRoot = stk.pop()
-        if(vis[c]):
-            continue  
-        vis[c] = True
-        if(isRoot):
-            cnt += 1
-            
-        for next_c, conn in enumerate(computers[c]):
-            if(conn==1):
-                stk.append((next_c, False))
+    for i in range(n):
+        cnt += dfs(i)
     
     return cnt
