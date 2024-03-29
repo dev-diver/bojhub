23cc8800e47e4b35da0f4e6ad98f7ddf238f2c28
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-03-29 14:33:27 +0900

    [Silver II] Title: 종이의 개수, Time: 728 ms, Memory: 158852 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.py
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -1,35 +1,30 @@
 import sys
 input = sys.stdin.readline
 N = int(input())
-MAP = [[*map(int, input().split(' '))] for _ in range(N)]
+M = [[*map(int, input().split(' '))] for _ in range(N)]
 
-def allSame(M):
-  e = M[0][0]
-  for line in M:
-    for elem in line:
-      if elem!=e: return False
+def allSame(x,y,k):
+  e = M[x][y]
+  for i in range(x,x+k):
+    for j in range(y,y+k):
+      if M[i][j] != e: return False
   return True
 
 def countElem(elem):
-  if elem ==-1:
-    cnt[0]+=1
-  elif elem == 0:
-    cnt[1]+=1
-  elif elem == 1:
-    cnt[2]+=1
+  cnt[elem+1]+=1
 
-def dfs(M,K):
-  if(K==1 or allSame(M)):
-    countElem(M[0][0])
+def dfs(x,y,k):
+  if(k==1 or allSame(x,y,k)):
+    countElem(M[x][y])
     return
-  section = [(0,K//3),(K//3,K//3*2),(K//3*2,K)]
-  for xs,xe in section:
-    for ys,ye in section:
-      m = [line[ys:ye] for line in M[xs:xe]]
-      dfs(m,K//3)
+  nk = k//3
+  section = [0,nk,nk*2]
+  for nx in section:
+    for ny in section:
+      dfs(x+nx,y+ny,nk)
 
 global cnt
 cnt = [0,0,0]
-dfs(MAP,N)
+dfs(0,0,N)
 
 print('\n'.join([*map(str,cnt)]))
