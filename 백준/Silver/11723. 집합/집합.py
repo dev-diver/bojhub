5963bf78f502c2e2e860e648e4ffe76c9a2d20d9
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-10 01:57:59 +0900

    [Silver V] Title: 집합, Time: 1388 ms, Memory: 121580 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,12 +1,8 @@
+import sys
+input=sys.stdin.readline
 M=int(input())
-batchSize = int(1e3)
-batch = M//batchSize
-remain = M%batchSize
-
 S=0
-# print(bin(S)[2:].zfill(20))
-def process(ans):
-  global S
+for _ in range(M):
   cmd,*x = input().strip().split()
   if(cmd=='all'):
     S=(1<<20)-1
@@ -19,19 +15,6 @@ def process(ans):
     elif(cmd=='remove'):
       S&=~(1<<x)
     elif(cmd=='check'):
-      ans.append((S>>x)&1)
+      sys.stdout.write(str((S>>x)&1)+'\n')
     elif(cmd=='toggle'):
       S^=(1<<x)
-
-for _ in range(batch):
-  ans=[]
-  for _ in range(batchSize):
-    process(ans)
-  if(ans): print(*ans, sep="\n")
-
-ans=[]
-for _ in range(remain):
-  process(ans)
-print(*ans, sep="\n")    
-  
-  
