2d576078a95ae18802801aff905fca7da4056362
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-09 16:28:58 +0900

    [Bronze I] Title: 팰린드롬수, Time: 104 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,6 +1,10 @@
 ans=[]
 while((w:=input().strip())!='0'):
-  k=''.join(reversed(w))
-  r = 'yes' if w==k else 'no'
-  ans.append(r)
+  flag='yes'
+  L=len(w)//2
+  for i in range(L) :
+      if w[i] != w[-1- i] :
+          flag = "no"
+          break
+  ans.append(flag)
 print(*ans,sep='\n')
