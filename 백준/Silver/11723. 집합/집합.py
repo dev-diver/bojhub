5ad10013fb16a385a011c600605128ce7a13b9a9
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-10 02:14:46 +0900

    [Silver V] Title: 집합, Time: 940 ms, Memory: 117440 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -4,15 +4,15 @@ M=int(input())
 S=0
 for _ in range(M):
   c = input().split()
-  if(c[0]=='all'):
-    S=(1<<21)-1
-  elif(c[0]=='empty'):
-    S=0
-  elif(c[0]=='add'):
-    S|=(1<<(int(c[1])))
+  if(c[0]=='add'):
+    S|=(1<<int(c[1]))
   elif(c[0]=='remove'):
     S&=~(1<<int(c[1]))
   elif(c[0]=='check'):
-    sys.stdout.write("1\n" if S & (1 << int(c[1])) else "0\n")
+    sys.stdout.write("1\n" if S&(1<<int(c[1])) else "0\n")
   elif(c[0]=='toggle'):
     S^=(1<<int(c[1]))
+  elif(c[0]=='all'):
+    S=(1<<21)-1
+  elif(c[0]=='empty'):
+    S=0
