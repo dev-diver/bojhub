f4a4f7a9ca8e892398fb7dde7b55c6c3b204fa39
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-10 02:11:49 +0900

    [Silver V] Title: 집합, Time: 952 ms, Memory: 117496 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,24 +1,18 @@
 import sys
-i=sys.stdin.readline
-l=[]
-for _ in range(int(i())):
-    c=i().split()
-    if c[0]=="add":
-        if c[1] in l:
-            continue
-        else:
-            l.append(c[1])
-    elif c[0]=="remove":
-        if c[1] in l:
-            l.remove(c[1])
-    elif c[0]=="check":
-        sys.stdout.write('1\n' if c[1] in l else '0\n')
-    elif c[0]=="toggle":
-        if c[1] in l:
-            l.remove(c[1])
-        else:
-            l.append(c[1])
-    elif c[0]=="all":
-        l=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20']
-    elif c[0]=="empty":
-        l=[]
+input=sys.stdin.readline
+M=int(input())
+S=0
+for _ in range(M):
+  c = input().split()
+  if(c[0]=='all'):
+    S=(1<<21)-1
+  elif(c[0]=='empty'):
+    S=0
+  elif(c[0]=='add'):
+    S|=(1<<(int(c[1])))
+  elif(c[0]=='remove'):
+    S&=~(1<<int(c[1]))
+  elif(c[0]=='check'):
+    sys.stdout.write("1\n" if S & (1 << int(c[1])) else "0\n")
+  elif(c[0]=='toggle'):
+    S^=(1<<int(c[1]))
