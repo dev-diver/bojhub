f50becc432c634dbe27bdbc02390dfd5fd45b3c6
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-14 00:59:49 +0900

    [Gold IV] Title: DSLR, Time: 9432 ms, Memory: 213220 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.py
+++ 백준/Gold/9019. DSLR/DSLR.py
@@ -5,21 +5,14 @@ T = int(input())
 
 def applyCmd(cmd, n:int):
   if(cmd=="D"):
-    n = 2*n%10000
+    n = n<<1
   elif(cmd=="S"):
     n = n-1
-    if(n==-1) : n=9999
-  else:
-    d4 = n % 10
-    d3 = n//10 % 10
-    d2 = n//100 % 10
-    d1 = n//1000 % 10
-    if(cmd=="L"):
-      d1,d2,d3,d4 = d2, d3, d4, d1
-    elif(cmd=="R"):
-      d1,d2,d3,d4 = d4, d1, d2, d3
-    n = d1*1000 + d2*100 + d3*10 + d4
-  return n
+  elif(cmd=="L"):
+    n = n*10+(n//1000)%10
+  elif(cmd=="R"):
+    n = n%10*1000 + n//10
+  return n%10000
 
 for _ in range(T):
   S,E = map(int, input().split())
