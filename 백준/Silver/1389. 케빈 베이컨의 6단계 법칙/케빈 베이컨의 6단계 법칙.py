5f8ec546060e28b063e6de687ad1f2bf1a6272d6
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-14 23:27:10 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 152 ms, Memory: 110892 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,25 +1,31 @@
 import sys
-import heapq
 input = sys.stdin.readline
 
 V,E = map(int, input().split())
-G = {v:[] for v in range(1,V+1)}
-for _ in range(E):
-  u,v = map(int,input().split())
-  G[u].append(v)
-  G[v].append(u)
+G = [[float('inf')]*V for _ in range(V)]
 
-m = (0,float('inf'))
-for s in range(1,V+1):
-  D=[0]+[float('inf')]*V
-  Q=[(0,s)]
-  while Q:
-    dist,u = heapq.heappop(Q)
-    if(D[u]!=float('inf')):
-      continue
-    D[u] = dist
-    for v in G[u]:
-      heapq.heappush(Q,(dist+1,v))
-  k = sum(D)
-  m = min(m, (s,k), key=lambda x:x[1])
+for _ in range(E):
+  u,v = map(lambda x:int(x)-1, input().split())
+  G[u][v] = 1
+  G[v][u] = 1
+
+D = [[float('inf')]*V for _ in range(V)]
+
+for i in range(V):
+  D[i][i] = 0
+
+for u in range(V):
+  for v in range(V):
+    D[u][v] = G[u][v]
+
+for k in range(V):
+  for u in range(V):
+    for v in range(V):
+      newD = D[u][k] + D[k][v]
+      if D[u][v] > newD:
+        D[u][v] = newD
+
+m = (0, float('inf'))
+for i in range(V):
+  m = min(m,(i+1,sum(D[i])), key=lambda x:x[1])
 print(m[0])
