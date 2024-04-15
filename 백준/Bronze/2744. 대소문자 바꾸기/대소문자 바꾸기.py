cd8acbf3905a69c0b0a95e39eea30e19025c670a
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-15 11:19:09 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
@@ -1,9 +1,1 @@
-diff = ord('a')-ord('A')
-def chg(x):
-  o = ord(x)
-  if 'a'<=x<='z':
-    return chr(o-diff)
-  else:
-    return chr(o+diff)
-S=[*map(chg, input().strip())]
-print(''.join(S))
+print(input().swapcase())
