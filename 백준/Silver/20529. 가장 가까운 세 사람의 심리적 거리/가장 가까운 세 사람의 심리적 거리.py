b2a4c10a497b11d15215afff916181125ecb2829
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-15 10:36:30 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 132 ms, Memory: 119572 KB -BaekjoonHub

--- 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.py
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.py
@@ -1,43 +1,32 @@
-import itertools
 import sys
 from itertools import combinations
 input = sys.stdin.readline
 
-def calDist(A,B):
-  dist = 0
-  for i in range(len(A)):
-    if(A[i]!=B[i]):
-      dist+=1
-  return dist
-
 def calThree(A,B,C):
-  s = 0
-  Comb = combinations([A,B,C],2)
-  for c in Comb:
-    s+=calDist(*c)
-  return s
-
+  a = set(A)
+  b = set(B)
+  c = set(C)
+  return len(a-b)+len(b-c)+len(c-a)
 
 T = int(input())
+
 for _ in range(T):
   N = int(input())
   P = input().strip().split()
-  S = dict()
-  for p in P:
-    if p in S:
-      S[p]+=1
-    else:
-      S[p]=1
-  if(max(S.values())>=3):
-    print(0)
+
+  if(N<=16):
+    m = 12
+    for k in combinations(P,3):
+      m = min(m,calThree(*k))
+    print(m)
     continue
-  K=[]
-  for s in S:
-    K.append(s)
-    if(S[s]!=1):
-      K.append(s)
-  Comb = combinations(K,3)
-  m = 12
-  for c in Comb:
-    m = min(m,calThree(*c))
-  print(m)
+
+  else:
+    ans = 2
+    d = {}
+    for p in P:
+      d[p] = d.get(p,0) + 1
+      if d[p] >=3:
+        ans = 0
+        break
+    print(ans)
