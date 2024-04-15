d72ef02cf36c97e9384149acc9a097a65e2b54f7
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-15 15:02:36 +0900

    [Gold III] Title: 아침 산책, Time: 604 ms, Memory: 157872 KB, Score: 108 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -2,29 +2,28 @@ from itertools import combinations
 import sys
 input = sys.stdin.readline
 N = int(input())
-F = [*map(int,[*input().strip()])]
-inner = [*map(lambda x:x[0],filter(lambda x: x[1]==1,enumerate(F)))]
+F = [*input().strip()]
 G = {i:[] for i in range(N)}
 for _ in range(N-1):
   u,v = map(int,input().split())
   G[u-1].append(v-1)
   G[v-1].append(u-1)
 
-def checkPath(s,e):
+def countPath(s):
   stk = [(s,-1)]
+  cnt = 0
   while(stk):
     v,p = stk.pop()
-    if(v==e):
-      return True
-    if(v!=s and F[v]==1):
-      continue
     for next_node in G[v]:
       if(next_node!=p):
-        stk.append((next_node,v))
-  return False
+        if(F[next_node]=='1'):
+          cnt+=1
+        else:
+          stk.append((next_node,v))
+  return cnt
 
-cnt=0
-for s,e in combinations(inner,2):
-  if checkPath(s,e):
-    cnt+=1
-print(cnt*2)
+cnt = 0
+for i in range(N):
+  if(F[i]=='1'):
+    cnt+=countPath(i)
+print(cnt)
