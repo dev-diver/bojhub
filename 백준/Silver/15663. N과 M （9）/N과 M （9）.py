365a0fe9a23ec9f1d8c2ea1e41c7862b8810a646
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-18 22:56:54 +0900

    [Silver II] Title: N과 M (9), Time: 136 ms, Memory: 113292 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -3,10 +3,10 @@ N,M = map(int,input().split())
 *S, = map(int,input().split())
 S.sort()
 
-ans = []
-for c in permutations(S,M):
-  c = ' '.join([*map(str,c)])
-  if(c not in ans):
-    ans.append(c)
+ans = set()
+for c in permutations(map(str,S),M):
+  c = ' '.join(c)
+  if c not in ans:
+    ans.add(c)
 
 print(*ans,sep='\n')
