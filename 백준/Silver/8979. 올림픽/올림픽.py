c54c3c9fca8fe6fdb166b4563536302635323b05
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-22 00:51:39 +0900

    [Silver V] Title: 올림픽, Time: 120 ms, Memory: 109240 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -2,9 +2,10 @@ import sys
 input = sys.stdin.readline
 N,K = map(int,input().split())
 M = [[*map(int, input().split())] for _ in range(N)]
-M.sort(key=lambda x:(x[1],x[2],x[3]))
-
-for i in range(1,N):
-  if(M[-i][0]==K):
-    print(i)
-    break
+M.sort(key=lambda x:(-x[1],-x[2],-x[3]))
+for i in range(N):
+  if(M[i][0]==K):
+    k=1
+    while(M[i][1:]==M[i-k][1:]):
+      k+=1
+    print(i-k+2)
