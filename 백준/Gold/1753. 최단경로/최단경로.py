610fe8c94919ea37fb574e9359f1206169589cce
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-22 15:14:18 +0900

    [Gold IV] Title: 최단경로, Time: 416 ms, Memory: 128004 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -6,19 +6,26 @@ K = int(input())
 G = {i:[] for i in range(1,N+1)}
 for _ in range(E):
   u,v,w = map(int,input().split())
-  G[u].append((v,w))
+  G[u].append(w<<16|v)
 
-D=[float('INF')]*(N+1)
+D=[sys.maxsize]*(N+1)
 D[K]=0
-Q=[(0,K)]
+Q=[K]
 while Q:
-  dist, v = heapq.heappop(Q)
+  n = heapq.heappop(Q)
+  dist = n>>16
+  v = n&0xffff
   if(dist > D[v]):
     continue
-  for next_v,next_dist in G[v]:
+  for next_n in G[v]:
+    next_dist = next_n>>16
+    next_v = next_n&0xffff
     distance = dist + next_dist
     if(distance < D[next_v]):
       D[next_v] = distance
-      heapq.heappush(Q,(distance,next_v))
-*D, = map(lambda x:x if x!=float('inf') else 'INF', D[1:])
-print(*D, sep='\n')
+      heapq.heappush(Q,distance<<16|next_v)
+for x in D[1:]:
+  if x!=sys.maxsize:
+    sys.stdout.write("%d\n"%x)
+  else:
+    sys.stdout.write("INF\n")
