1fe97959113f8408f9b366d820cb7aca943945e7
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-22 17:44:43 +0900

    [Gold V] Title: 택배 배송, Time: 260 ms, Memory: 120712 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.py
+++ 백준/Gold/5972. 택배 배송/택배 배송.py
@@ -1,22 +1,26 @@
-import sys,heapq
-input = sys.stdin.readline
+import sys,heapq,io,os
+input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
 N,M=map(int,input().split())
 G = {i:[] for i in range(1,N+1)}
 for i in range(M):
   u,v,w = map(int,input().split())
-  G[u].append((w,v))
-  G[v].append((w,u))
+  G[u].append(w<<16|v)
+  G[v].append(w<<16|u)
 
 D=[sys.maxsize]*(N+1)
 D[1]=0
-Q=[(0,1)]
+Q=[1]
 while Q:
-  dist,n = heapq.heappop(Q)
+  q = heapq.heappop(Q)
+  dist = q>>16
+  n = q&0xffff
   if(D[n] < dist):
     continue
-  for w,v in G[n]:
+  for next in G[n]:
+    w = next>>16
+    v = next&0xffff
     distance = dist+w
     if(D[v] > distance):
       D[v] = distance
-      heapq.heappush(Q,(distance,v))
+      heapq.heappush(Q,distance<<16|v)
 print(D[-1])
