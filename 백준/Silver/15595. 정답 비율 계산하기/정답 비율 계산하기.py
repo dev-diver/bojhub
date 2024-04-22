421cc6f1282fe747005636f6f3b01b89157e5f95
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-23 00:36:16 +0900

    [Silver III] Title: 정답 비율 계산하기, Time: 188 ms, Memory: 121104 KB -BaekjoonHub

--- 백준/Silver/15595. 정답 비율 계산하기/정답 비율 계산하기.py
+++ 백준/Silver/15595. 정답 비율 계산하기/정답 비율 계산하기.py
@@ -1,18 +1,18 @@
-import sys
-input = sys.stdin.readline
+import sys,io,os
+input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
 T = int(input())
 correct={}
 wrong={}
 cnt=0
 for _ in range(T):
   _,user,case,_,_,_,_ = input().strip().split()
-  if(user=='megalusion' or user in correct):
+  if(user==b'megalusion' or user in correct):
     continue
-  if(case=='4'):
+  if(case==b'4'):
     wr = wrong.get(user,0)
     cnt += wr
     correct[user] = wr
   else:
     wrong[user] = wrong.get(user,0)+1
 ans = len(correct)/(len(correct)+cnt)*100 if len(correct)>0 else 0
-print(ans)
+sys.stdout.write(str(ans))
