8f58cebf5a77f36b443aeef23022347081ec7b58
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-23 19:28:19 +0900

    [Gold III] Title: 내리막 길, Time: 184 ms, Memory: 117112 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,23 +1,23 @@
-import sys,io,os
+import sys, io, os
 import heapq
-input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
-M,N = map(int,input().split())
-G = [[*map(int,input().split())] for _ in range(M)]
+input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+M, N = map(int, input().split())
+G = [[*map(int, input().split())] for _ in range(M)]
 D = [[0]*N for _ in range(M)]
-D[0][0]=1
-Q = [(-G[0][0],0,0)]
-directions = [(0,1),(0,-1),(1,0),(-1,0)]
+D[0][0] = 1
+Q = [(-G[0][0], 0, 0)]
+directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 while Q:
-  H,x,y = heapq.heappop(Q)
-  if(not (x==0 and y==0) and D[x][y]>0):
-    continue
-  H*=-1
-  for dx,dy in directions:
-    nx,ny = x+dx,y+dy
-    if(0<=nx<M and 0<=ny<N):
-      h = G[nx][ny]
-      if(h<H):
-        heapq.heappush(Q, (-h,nx,ny))
-      elif(h>H):
-        D[x][y]+=D[nx][ny]
+    H, x, y = heapq.heappop(Q)
+    H = -H
+    for d in directions:
+        nx = x+d[0]
+        ny = y+d[1]
+        if 0 <= nx < M and 0 <= ny < N:
+            h = G[nx][ny]
+            if h < H:
+                if D[nx][ny] == 0:
+                    heapq.heappush(Q, (-h, nx, ny))
+                D[nx][ny] += D[x][y]
+
 sys.stdout.write(str(D[M-1][N-1]))
