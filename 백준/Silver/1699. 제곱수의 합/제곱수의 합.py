dea0b55a9a770077065254ebddb9203b011709e4
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-24 20:04:39 +0900

    [Silver II] Title: 제곱수의 합, Time: 176 ms, Memory: 111056 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -1,15 +1,11 @@
 N = int(input())
-S = [i*i for i in range(1,317)]
+S = [i*i for i in range(1,318)]
 dp = [0]*(N+1)
-dp[1] = 1
+dp[1]=1
 for i in range(2,N+1):
-  mn = i
+  mn=i
   for s in S:
-    if(i==s):
-      mn = 1
-      break
-    if(i-s<0):
-      break
+    if(i-s<0):break
     mn = min(mn, 1 + dp[i-s])
-  dp[i] = mn
+  dp[i]=mn
 print(dp[-1])
