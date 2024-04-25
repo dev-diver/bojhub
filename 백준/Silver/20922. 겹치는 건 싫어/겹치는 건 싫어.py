26aaf2d9762c4bd044359f893a22611bff10ca83
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-25 12:41:54 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 200 ms, Memory: 149020 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
@@ -5,14 +5,11 @@ L = 0
 maxL = 0
 for i in range(N):
   s = S[i]
-  cnt = D.get(s,0) +1
-  if(cnt > K):
-    maxL = max(L,maxL)
-    while(D[s]+1>K):
-      k = S[i-L]
-      D[k]-=1
-      L-=1
+  while(D.get(s,0)+1>K):
+    k = S[i-L]
+    D[k]-=1
+    L-=1
   D[s] = D.get(s,0)+1
   L+=1
-maxL = max(L,maxL)
+  maxL = max(L,maxL)
 print(maxL)
