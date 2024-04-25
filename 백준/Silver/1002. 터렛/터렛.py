a392fe42a7233b792ed1e646c093028150490f6c
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-25 22:25:41 +0900

    [Silver III] Title: 터렛, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.py
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -1,12 +1,17 @@
+import os,io,sys
+input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
+output = io.StringIO()
 T=int(input())
+ans = []
 for _ in range(T):
     x1,y1,r1,x2,y2,r2 = map(int,input().split())
-    d = ((x1-x2)**2 + (y1-y2)**2)**0.5
-    if(x1==x2 and y1==y2 and r1==r2):
-        print(-1)
-    elif(d==r1+r2 or d==abs(r1-r2)):
-        print(1)
-    elif(d<abs(r1-r2) or d>r1+r2):
-        print(0)
+    d = (x1-x2)**2 + (y1-y2)**2
+    if(d==0 and r1==r2):
+        output.write("-1\n")
+    elif(d==(r1+r2)**2 or d==(r1-r2)**2):
+        output.write("1\n")
+    elif(d<(r1-r2)**2 or d>(r1+r2)**2):
+        output.write("0\n")
     else:
-        print(2)
+        output.write("2\n")
+sys.stdout.write(output.getvalue())
