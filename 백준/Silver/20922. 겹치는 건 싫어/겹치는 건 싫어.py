6744c3f85864ed994dd263db4aac050d6a78096d
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-25 12:45:29 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 156 ms, Memory: 141544 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
@@ -1,15 +1,17 @@
+import io,os,sys
+input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
 N,K = map(int,input().split())
 *S, = map(int,input().split())
-D = dict()
+D = [0]*100_001
 L = 0
 maxL = 0
 for i in range(N):
   s = S[i]
-  while(D.get(s,0)+1>K):
+  while(D[s]+1>K):
     k = S[i-L]
     D[k]-=1
     L-=1
-  D[s] = D.get(s,0)+1
+  D[s] += 1
   L+=1
   maxL = max(L,maxL)
-print(maxL)
+sys.stdout.write(str(maxL))
