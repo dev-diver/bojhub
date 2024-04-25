378655dc86fa2b9ad0ef2e9b8e69f8b0c317df04
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-26 01:10:18 +0900

    [Gold IV] Title: LCS 3, Time: 184 ms, Memory: 120052 KB -BaekjoonHub

--- 백준/Gold/1958. LCS 3/LCS 3.py
+++ 백준/Gold/1958. LCS 3/LCS 3.py
@@ -1,16 +1,16 @@
 import sys
 input = sys.stdin.readline
-A=' '+input().rstrip()
-B=' '+input().rstrip()
-C=' '+input().rstrip()
-lenA = len(A)
-lenB = len(B)
-lenC = len(C)
+A=input().rstrip()
+B=input().rstrip()
+C=input().rstrip()
+lenA = len(A)+1
+lenB = len(B)+1
+lenC = len(C)+1
 dp = [[[0]*lenC for _ in range(lenB)] for _ in range(lenA)]
 for i in range(1,lenA):
     for j in range(1,lenB):
         for k in range(1,lenC):
-            if(A[i]==B[j] and B[j]==C[k]):
+            if(A[i-1]==B[j-1]==C[k-1]):
                 dp[i][j][k] = dp[i-1][j-1][k-1]+1
             else:
                 dp[i][j][k] = max(dp[i-1][j][k],dp[i][j-1][k],dp[i][j][k-1])
