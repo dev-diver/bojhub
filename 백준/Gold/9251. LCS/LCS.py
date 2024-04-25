6cca32804546d8a2ffc5dbf6c8ad79d6c868290b
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-26 00:05:41 +0900

    [Gold V] Title: LCS, Time: 148 ms, Memory: 114724 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,16 +1,17 @@
 import sys
 input = sys.stdin.readline
+*A, = input().strip()
+*B, = input().strip()
+A=[0]+A
+B=[0]+B
 
-A = input().strip()
-B = input().strip()
-
-dp = [[0] * (len(B) + 1) for _ in range(len(A) + 1)]
-
-for i in range(1, len(A) + 1):
-    for j in range(1, len(B) + 1):
-        if A[i - 1] == B[j - 1]:
-            dp[i][j] = dp[i - 1][j - 1] + 1
+memo=[[0]*(len(B)) for _ in range(len(A))]
+for i in range(1,len(A)):
+    for j in range(1,len(B)):
+        r=0
+        if(A[i]==B[j]):
+            r=1+memo[i-1][j-1]
         else:
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-
-print(dp[len(A)][len(B)])
+            r=max(memo[i-1][j],memo[i][j-1])
+        memo[i][j]=r
+print(memo[len(A)-1][len(B)-1])
