3d8723079d547fba9f4e383e53fd1b95b263a245
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-30 00:41:21 +0900

    [Silver II] Title: 아기 상어 2, Time: 152 ms, Memory: 114024 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
@@ -3,32 +3,24 @@ from collections import deque
 input = sys.stdin.readline
 N,M=map(int,input().split())
 G = [[*map(int,input().split())] for _ in range(N)]
-D = [[M+N]*M for _ in range(N)]
+V = [[False]*M for _ in range(N)]
 directions = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,-1),(-1,1)]
-def getSafe(coord):
-    cx,cy = coord
-    Q = deque([])
-    Q.append((cx,cy,0))
-    D[cx][cy] = 0
+def getSafe(Q):
     result = 0
     while Q:
         x,y,dist = Q.popleft()
         result = dist
         for dx,dy in directions:
             nx,ny = x+dx,y+dy
-            if(0<=nx<N and 0<=ny<M and D[nx][ny]>dist+1 and G[nx][ny]!=1):
-                D[nx][ny] = dist+1
+            if(0<=nx<N and 0<=ny<M and not V[nx][ny]):
                 Q.append((nx,ny,dist+1))
+                V[nx][ny]=True
     return result
 
-
+Q=deque([])
 for i in range(N):
     for j in range(M):
         if(G[i][j]==1):
-            getSafe((i,j))
-
-mx=0
-for d in D:
-    mx=max(mx,max(d))
-
-print(mx)
+            Q.append((i,j,0))
+            V[i][j]=True
+print(getSafe(Q))
