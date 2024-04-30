a495f0ddaed155639e308d8d0f640e692decd8f8
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-04-30 13:08:06 +0900

    [Gold V] Title: 평범한 배낭, Time: 728 ms, Memory: 198500 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -3,15 +3,18 @@ input = sys.stdin.readline
 N,K = map(int,input().split())
 I = [[*map(int, input().split())] for _ in range(N)] #W, V
 I = [[0,0]]+I
-memo = [[0]*(K+1) for _ in range(2)]
+MaxV=K+1
+memo = [[0]*len(I) for _ in range(MaxV)]
 
-for itemIdx in range(len(I)):
-    for capacity in range(K+1):  
-        ip=itemIdx%2
-        r = memo[ip-1][capacity] #아이템 안 썼을 떄
-        if capacity - I[itemIdx][0] >=0 :
+for capacity in range(K+1):
+    for itemIdx in range(1,len(I)): 
+        capI=capacity%MaxV
+        if itemIdx > 0:
+            r = memo[capI][itemIdx-1] #아이템 안 썼을 때
+        restCap = capacity - I[itemIdx][0]
+        if  restCap >=0 :
             #아이템 썼을 때랑 비교
-            r = max(r,I[itemIdx][1]+memo[ip-1][capacity - I[itemIdx][0]])
-        memo[ip][capacity]=r
+            r = max(r,I[itemIdx][1]+memo[restCap%MaxV][itemIdx-1])
+        memo[capI][itemIdx]=r
 
-print(memo[len(I)%2-1][K])
+print(memo[K%MaxV][len(I)-1])
