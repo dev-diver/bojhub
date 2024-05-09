70268f2cf0c4c4964072bda3f3a5e0ccc05118f5
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-05-09 14:22:01 +0900

    [level 2] Title: 올바른 괄호, Time: 6.61 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,15 +1,11 @@
 def solution(s):
-    answer = True
     stk=[]
     for c in s:
         if(c=="("):
             stk.append(c)
         else:
-            if(stk and stk[-1]=="("):
+            try:
                 stk.pop()
-            else:
-                answer = False
-                break
-    if stk:
-        answer = False
-    return answer 
+            except IndexError:
+                return False
+    return False if stk else True 
