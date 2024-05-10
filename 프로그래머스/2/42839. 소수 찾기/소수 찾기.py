24bc72e44b33d23970a59437a1512ec4dba7090d
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-05-10 17:01:49 +0900

    [level 2] Title: 소수 찾기, Time: 9.26 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,12 +1,13 @@
 def solution(numbers):
     Num = set()
     
-    def comb(Num,pick,unpick):
+    def comb(pick,unpick):
         if(pick!=''):
-            Num|={int(pick)}
+            Num.add(int(pick))
         for i in range(len(unpick)):
-            comb(Num,pick+unpick[i], unpick[:i]+unpick[i+1:])
-    comb(Num,'',numbers)
+            comb(pick+unpick[i], unpick[:i]+unpick[i+1:])
+            
+    comb('',numbers)
     Num-={0,1}
     for i in range(2,int(max(Num)**0.5)+1):
         Num-=set(range(i*2,max(Num)+1,i))
