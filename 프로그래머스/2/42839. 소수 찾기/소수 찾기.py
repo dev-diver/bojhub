2feb61c99ef832cc2355cad35ceae8925ac56ca6
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-05-10 16:47:55 +0900

    [level 2] Title: 소수 찾기, Time: 5.64 ms, Memory: 13.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,38 +1,11 @@
 from itertools import permutations
-
 def solution(numbers):
-    
-    MAX=int(1e4)
-    Num = [True]*MAX
-    Num[0], Num[1] = False, False
-    primes=[]
-    
-    answer = 0
-    for i in range(2,MAX):
-        if(Num[i]==True):
-            primes.append(i)
-            k=2
-            while(i*k<MAX):
-                Num[i*k]=False
-                k+=1
-    
-    nums = [*numbers]
-    combs = set()
-    for l in range(1,len(nums)+1):
-        for perm in permutations(nums,l):
-            combs.add(int(''.join(perm)))
-            
-    def isPrime(N):
-        for p in primes:
-            if(N%p==0):
-                return False
-        return True
-    
-    for c in combs:
-        try:
-            if(Num[c]):
-                answer+=1
-        except IndexError:
-            if(isPrime(c)):
-                answer+=1
-    return answer
+    Num = set()
+    numbers = [*numbers]
+    for i in range(len(numbers)):
+        for perm in permutations(numbers,i+1):
+            Num|={int(''.join(perm))}
+    Num-={0,1}
+    for i in range(2,int(max(Num)**0.5)+1):
+        Num-=set(range(i*2,max(Num)+1,i))
+    return len(Num)
