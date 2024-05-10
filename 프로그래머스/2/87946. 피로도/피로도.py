468fd55ae878c8cc43807156212d4de5b206daa9
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-05-10 20:18:44 +0900

    [level 2] Title: 피로도, Time: 0.11 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,18 +1,18 @@
 def solution(k, dungeons):
     
     visit=[False]*len(dungeons)
-    def dfs(k,cnt,dungeons):
+    def dfs(k,cnt):
         if(cnt>=len(dungeons)):
             return cnt
         result = cnt
         for i in range(len(dungeons)):
             need, cost = dungeons[i]
             if(visit[i]==False and k>=need):
-                visit[i] = True
-                result = max(result, dfs(k-cost,cnt+1,dungeons))
+                visit[i] = True  #visit 상태를 전달하지 않고 True, False만 Toggle
+                result = max(result, dfs(k-cost,cnt+1))
                 visit[i] = False
         return result
 
-    mx = dfs(k,0,dungeons)
+    mx = dfs(k,0)
     
     return mx
