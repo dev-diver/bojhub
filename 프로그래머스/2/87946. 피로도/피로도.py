a6fc3bfd43d61c5d3cccee356954c98e177a5dc4
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-05-10 19:22:19 +0900

    [level 2] Title: 피로도, Time: 0.23 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,9 +1,17 @@
-from itertools import permutations
-
 def solution(k, dungeons):
+    def makePerm(make,last):
+        if(len(last)==0):
+            perms.append(make)
+            return
+        else:
+            for i in range(len(last)):
+                makePerm(make+[last[i]],last[:i]+last[i+1:])
+    
+    perms = []
+    makePerm([],dungeons)
     
     mx = 0
-    for perm in permutations(dungeons, len(dungeons)):
+    for perm in perms:
         hp = k
         visit = 0
         for need,cost in perm:
