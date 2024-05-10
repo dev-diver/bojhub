1851a6a06b03c010dc5aee0b52f8ed3a06f059eb
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-05-11 00:30:54 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.32 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,3 +1,24 @@
 def solution(n, wires):
-    answer = -1
-    return answer
+    G = {e:[] for e in range(1,n+1)}
+    for v, u in wires:
+        G[v].append(u)
+        G[u].append(v)
+        
+    def dfs(V, cut):
+        visited = [False]*(n+1)
+        visited[V] = True
+        visited[cut] = True
+        stk=[V]
+        while(stk):
+            v = stk.pop()
+            for nextV in G[v]:
+                if(not visited[nextV]):
+                    visited[nextV] = True
+                    stk.append(nextV)
+        return sum(1 for x in visited if x)-1
+        
+    mn = n
+    for v1,v2 in wires:
+        temp = abs(n-2*dfs(v1,v2))
+        mn = min(mn, temp)
+    return mn
