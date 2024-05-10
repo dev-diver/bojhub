d19c79bc386f447593edc90a498810c9d398ede1
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-05-10 15:14:48 +0900

    [level 1] Title: 모의고사, Time: 8.34 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,24 +1,18 @@
-def one(i):
-    return i%5+1
 
-pattern2=[2,1,2,3,2,4,2,5,2]
-def two(i):
-    i=i%8
-    return pattern2[i]
-
-pattern3=[3,3,1,1,2,2,4,4,5,5]
-def three(i):
-    i=i%10
-    return pattern3[i]
-    
 def solution(answers):
+    pattern=[]
+    pattern.append([1,2,3,4,5])
+    pattern.append([2,1,2,3,2,4,2,5])
+    pattern.append([3,3,1,1,2,2,4,4,5,5])
+    
     answer = []
-    cnt=[0,0,0]
+    cnt = [0,0,0]
     for i in range(len(answers)):
-        cnt[0]+=1 if one(i)==answers[i] else 0
-        cnt[1]+=1 if two(i)==answers[i] else 0
-        cnt[2]+=1 if three(i)==answers[i] else 0
+        for j in range(len(cnt)):
+            cnt[j]+=1 if pattern[j][i%len(pattern[j])]==answers[i] else 0
+    
     maxAns = max(cnt)
+    
     for i in range(3):
         if(cnt[i]==maxAns):
             answer.append(i+1)
