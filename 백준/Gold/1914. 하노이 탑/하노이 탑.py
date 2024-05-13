10b5a97c2b516c92e5bdf073be3115baa9e1a7d2
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-05-14 01:02:16 +0900

    [Gold V] Title: 하노이 탑, Time: 56 ms, Memory: 53064 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.py
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.py
@@ -1,4 +1,8 @@
+import sys,io
+input = sys.stdin.readline
+output = io.StringIO()
 d = {}
+a = int(input())
 def move(a, b, c):
     if a == 1:
         return f'{b} {c}\n' 
@@ -6,7 +10,7 @@ def move(a, b, c):
         return d[(a, b)]  
     d[(a, b)] = ''.join([move(a-1, b, 6-b-c), f'{b} {c}\n', move(a-1, 6-b-c, c)])
     return d[(a, b)]
-a = int(input())
-print(pow(2,a)-1)
+output.write(f'{(1<<a)-1}\n')
 if a <= 20:
-    print(move(a, 1, 3))
+    output.write(f'{move(a, 1, 3)}')
+sys.stdout.write(output.getvalue())
