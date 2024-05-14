4563b0652f83b629b5e9ffba5ae0912a0009e0b6
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-05-14 10:21:42 +0900

    [level 2] Title: 의상, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -3,5 +3,5 @@ from functools import reduce
 from operator import mul
 def solution(clothes):
     c = Counter(x[1] for x in clothes)
-    m = reduce(mul,[c[e]+1 for e in c])
+    m = reduce(mul,[v+1 for v in c.values()])
     return m-1
